f76927d5f86697dc80b02bfb55da623f5202954f
user <nobody>
2017-08-14 04:13:13 +0000

    symliiiiinks

--- string.h
+++ string.h
@@ -1,1 +1,26 @@
-../../generate.c/source/string.h
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#include <stdlib.h> // size_t
+
+typedef struct mstring {
+	char* s;
+	size_t l;
+} mstring;
+
+/* because C sucks, const string str; str.s has type char* not const char*
+	 and there's no way to conditionally make it const char*
+ */
+typedef struct string {
+	const char* s;
+	size_t l;
+} string;
+
+#define STRPRINT(str) fwrite((str).s,(str).l,1,stdout);
+
+#define LITSIZ(a) (sizeof(a)-1)
+#define LITLEN(a) a,LITSIZ(a)
+
+#define CSTR(a) *((struct string*)&a)
+
+#endif /* _STRING_H_ */
