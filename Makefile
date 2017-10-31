aa6359b293b4c624c80d911ef544b0ee700968db
autocommit <autocommit>
2017-10-31 06:43:33 +0000

    auto 0 15 138

--- Makefile
+++ Makefile
@@ -1,36 +1,13 @@
 include coolmake/head.mk
-
-o/wanted_tags.lo: o/%.lo: o/%.gen.c
-	$(COMPILE)
-
-$(call $(AUTOMAKE_SUBPROJECT), libxml2, libxml2)
-
-o/libxmlfixes.lo: | libxml2/include
-CFLAGS+=-Ilibxml2/include
-
-coolmake/head.mk: git-tools/funcs.sh coolmake/tail.mk libxml2/include
-	sh setup.sh # $@
-
-coolmake/tail.mk libxml2/include: ;
+include main.mk
+include coolmake/tail.mk
 
 git-tools/funcs.sh:
 	git submodule update --init
 
-N=wanted_tags libxmlfixes
-libxmlfixes.la: $O 
-	$(LINK)
 
-N=make-wanted
-o/make-wanted: $O
-	$(LINK)
-
-o/make-wanted.lo: make-wanted.c | o
-	$(COMPILE)
-
-o/wanted_tags.gen.c o/wanted_tags.gen.h: o/make-wanted tags.wanted | o
-	$(call STATUS,Generate,wanted_tags)
-	$(S)cd o && ../$(firstword $^) < ../$(lastword $^)
-
-include coolmake/tail.mk
+coolmake/head.mk:
+	sh setup.sh
+coolmake/tail.mk libxml2/include: ;
 
 all: libxmlfixes.la
--- main.mk
+++ main.mk
@@ -1,36 +1,25 @@
-include coolmake/head.mk
-
-o/wanted_tags.lo: o/%.lo: o/%.gen.c
+$(TOP)o/wanted_tags.lo: o/%.lo: o/%.gen.c
 	$(COMPILE)
 
 $(call $(AUTOMAKE_SUBPROJECT), libxml2, libxml2)
 
-o/libxmlfixes.lo: | libxml2/include
+$(TOP)o/libxmlfixes.lo: | libxml2/include
 CFLAGS+=-Ilibxml2/include
 
 coolmake/head.mk: git-tools/funcs.sh coolmake/tail.mk libxml2/include
-	sh setup.sh # $@
-
-coolmake/tail.mk libxml2/include: ;
-
-git-tools/funcs.sh:
-	git submodule update --init
 
 N=wanted_tags libxmlfixes
-libxmlfixes.la: $O 
+$(TOP)libxmlfixes.la: $O 
 	$(LINK)
 
 N=make-wanted
-o/make-wanted: $O
+$(TOP)o/make-wanted: $O
 	$(LINK)
 
-o/make-wanted.lo: make-wanted.c | o
+$(TOP)o/make-wanted.lo: make-wanted.c | o
 	$(COMPILE)
 
-o/wanted_tags.gen.c o/wanted_tags.gen.h: o/make-wanted tags.wanted | o
+$(TOP)o/wanted_tags.gen.c o/wanted_tags.gen.h: o/make-wanted tags.wanted | o
 	$(call STATUS,Generate,wanted_tags)
 	$(S)cd o && ../$(firstword $^) < ../$(lastword $^)
 
-include coolmake/tail.mk
-
-all: libxmlfixes.la
