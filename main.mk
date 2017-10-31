64720ae752547f67a14019f43733af95e9545888
autocommit <autocommit>
2017-10-31 07:10:34 +0000

    auto 0 16 196

--- main.mk
+++ main.mk
@@ -1,25 +1,29 @@
-$(TOP)o/wanted_tags.lo: o/%.lo: o/%.gen.c
-	$(COMPILE)
+include coolmake/top.mk
+
+OUT=wanted_tags
+N=wanted_tags.gen
+$(eval $(OBJECT))
 
 $(call $(AUTOMAKE_SUBPROJECT), libxml2, libxml2)
 
-$(TOP)o/libxmlfixes.lo: | libxml2/include
+$(O)/libxmlfixes.lo: | libxml2/include
 CFLAGS+=-Ilibxml2/include
 
 coolmake/head.mk: git-tools/funcs.sh coolmake/tail.mk libxml2/include
 
-N=wanted_tags libxmlfixes
-$(TOP)libxmlfixes.la: $O 
-	$(LINK)
+N:=wanted_tags libxmlfixes
+OUT:=libxmlfixes.la
+$(eval $(PROGRAM))
 
 N=make-wanted
-$(TOP)o/make-wanted: $O
-	$(LINK)
+OUT=make-wanted
+$(eval $(PROGRAM))
 
-$(TOP)o/make-wanted.lo: make-wanted.c | o
-	$(COMPILE)
+N=make-wanted
+OUT=make-wanted
+$(eval $(OBJECT))
 
-$(TOP)o/wanted_tags.gen.c o/wanted_tags.gen.h: o/make-wanted tags.wanted | o
+$(O)/wanted_tags.gen.c $(O)/wanted_tags.gen.h: $(O)/make-wanted tags.wanted | $(O)
 	$(call STATUS,Generate,wanted_tags)
 	$(S)cd o && ../$(firstword $^) < ../$(lastword $^)
 
