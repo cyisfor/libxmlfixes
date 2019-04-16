509e8908b426f53d58725c5f152dfe5211fa9c63
autocommit <autocommit>
2019-04-16 06:54:43 +0000

    auto 0 686 8200

--- suck_config.py
+++ suck_config.py
@@ -2,13 +2,20 @@ import sys,re
 
 class pat:
 	undef = re.compile("^#undef (.*)")
-
+	comment = re.compile(r'/\* *(.*?) *\*/')
 print(sys.stdin.readline())
 print("")
 
 for comment in sys.stdin:
+	m = pat.comment.match(comment)
+	if not m: continue
+	comment = m.group(1)
 	undef = sys.stdin.readline()
-	name = pat.undef.match(undef).group(1)
+	m = pat.undef.match(undef)
+	if not m:
+		print('undef',repr(undef))
+		continue
+	name = m.group(1)
 	comment = comment[2:-2].strip()
 	print("set("+name+" CACHE BOOL OFF "+repr(comment)+")")
 	
