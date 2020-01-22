5ee6e9d53f9154b638fb41dfb03d746e583482f3
user <email>
2020-01-22 07:02:15 +0000

    moving to repo

--- src/libxmlfixes.h
+++ src/libxmlfixes.h
@@ -17,3 +17,11 @@ xmlChar* findPropCsux(xmlNode* o, xmlChar* name, size_t len);
 xmlNode* nextE(xmlNode* e);
 // where the hell did this go?
 void html_dump_to_fd(int fd, xmlDoc* doc);
+
+/* To walk a tree,
+   1) next, next, next, ... until next is null
+   2) if child is not null, go down, then goto (1)
+   3) next is null, child is null, so go parent then...
+   4) if prev is not null, prev, child, then goto (1)
+   5) next, child, prev is null, go parent
+ */
